c16624e29c5088a824f72f2e7e09f267b9f239fa
Shotaro Sasaki <fspia.biz@gmail.com>
2016-07-10 06:50:14 +0900

    chat template

--- chat/xyz/skycat/main.go
+++ chat/xyz/skycat/main.go
@@ -3,21 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
+	"path/filepath"
+	"sync"
 )
 
-func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-					チャットしましょう！
-				</body>
-			</html>
-		`))
+type templateHandler struct {
+	once sync.Once
+	filename string
+	templ *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func(){
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main(){
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	err := http.ListenAndServe(":5050", nil)
 	if err != nil {
